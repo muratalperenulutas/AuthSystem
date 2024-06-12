9d9dd242a051bced9c7be31abd4a3cba9b8c695f
Murat Alperen Ulutaş <153949398+muratalperenulutas@users.noreply.github.com>
2024-06-12 18:16:02 +0300

    added try catch

--- routes/login.js
+++ routes/login.js
@@ -5,7 +5,8 @@ const User = require("../models/user");
 router.post("/", async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await User.findByEmail(email);
+  try{
+    const user = await User.findByEmail(email);
 
   if (!user) {
     return res.status(404).json({ message: "User not found" });
@@ -16,6 +17,12 @@ router.post("/", async (req, res) => {
   }
 
   res.status(200).json({ message: "Login successful" });
+    
+  }
+  catch(error){
+    res.status(500).json({ message: "Internal server error" });
+  }
+  
 });
 
 module.exports = router;
--- routes/register.js
+++ routes/register.js
@@ -5,14 +5,22 @@ const User = require("../models/user");
 router.post("/", async (req, res) => {
   const { email, password } = req.body;
 
-  const existingUser = await User.findByEmail(email);
+  try {
+    const existingUser = await User.findByEmail(email);
 
-  if (existingUser) {
-    return res.status(400).json({ message: "User already exist!" });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exist!" });
+    }
+  
+    const newUser = await User.registerUser(email, password);
+    res.status(201).json({ message: "User has been registered!", user: newUser });
+    
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", user: newUser });
+    
   }
 
-  const newUser = await User.registerUser(email, password);
-  res.status(201).json({ message: "User has been registered!", user: newUser });
+ 
 });
 
 module.exports = router;
