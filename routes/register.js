1b0a4c01aba48582de74a4a3e4bfcdb404aa5775
Murat Alperen Ulutaş <153949398+muratalperenulutas@users.noreply.github.com>
2024-06-12 20:17:38 +0300

    fixed register.js

--- routes/register.js
+++ routes/register.js
@@ -6,22 +6,25 @@ router.post("/", async (req, res) => {
   const { username, email, password } = req.body;
 
   try {
-    const existingUser = await User.findByEmail(email);
+    const existingUser1 = await User.findByEmail(email);
+    const existingUser2 = await User.findByUsername(username);
 
-    if (existingUser) {
-      return res.status(400).json({ message: "User already exist!" });
+    if (existingUser1) {
+      return res.status(400).json({ message: "Email already in use!" });
     }
-  
-    const newUser = await User.registerUser(username, email, password);
-    res.status(201).json({ message: "User has been registered!", user: newUser });
-    
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: "Internal server error"});
-    
-  }
 
- 
+    if (existingUser2) {
+      return res.status(400).json({ message: "Username already in use!" });
+    }
+
+    const newUser = await User.registerUser(username, email, password);
+    res
+      .status(201)
+      .json({ message: "User has been registered!", user: newUser });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 module.exports = router;
